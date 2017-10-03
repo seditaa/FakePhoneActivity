c6f38f4950702ae870705d3995ea4dd26750893c
seditaa <daniel.martinez.g1@estudiant.upc.edu>
2017-10-03 12:44:46 +0200

    Empezamos la app FakePhoneActivity.

---
+++ app/src/main/java/edu/upc/eseiaat/pma/fakephoneactivity/PhoneActivity.java
@@ -0,0 +1,13 @@
+package edu.upc.eseiaat.pma.fakephoneactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhoneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone);
+    }
+}
